3c63ef85713764cc4e3cf4f8a730380d4f6f2da7
jeongmin <jeongmin010324@gmail.com>
2025-01-16 19:26:08 +0900

    feat : point module에 필요한 기능들 export 및 dto 생성 메서드 추가

--- website_be/src/mypage/mypage.module.ts
+++ website_be/src/mypage/mypage.module.ts
@@ -1,14 +1,15 @@
-import { Module } from "@nestjs/common";
-import { TypeOrmModule } from "@nestjs/typeorm";
-import { MypageController } from "./mypage.controller";
-import { MypageService } from "./service/mypage.service";
-import { HistoryRepository } from "./repository/history.repository";
-import { History } from "./entities/history.entity";
-import { UserModule } from "src/user/user.module"; 
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { MypageController } from './mypage.controller';
+import { MypageService } from './service/mypage.service';
+import { HistoryRepository } from './repository/history.repository';
+import { History } from './entities/history.entity';
+import { UserModule } from 'src/user/user.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([History]), UserModule], 
+  imports: [TypeOrmModule.forFeature([History]), UserModule],
   controllers: [MypageController],
-  providers: [MypageService, HistoryRepository], 
+  providers: [MypageService, HistoryRepository],
+  exports: [HistoryRepository],
 })
 export class MypageModule {}
