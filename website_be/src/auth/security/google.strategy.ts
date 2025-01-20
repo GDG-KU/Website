ca06a41f64a006c39059448d568c11a807164ccc
wintery39 <wintery39@naver.com>
2025-01-20 11:21:41 +0900

    release: hard coding

--- website_be/src/auth/security/google.strategy.ts
+++ website_be/src/auth/security/google.strategy.ts
@@ -9,7 +9,8 @@ export class GoogleStrategy extends PassportStrategy(Strategy, 'google') {
     super({
       clientID: configService.get<string>('GOOGLE_CLIENT_ID'),
       clientSecret: configService.get<string>('GOOGLE_CLIENT_SECRET'),
-      callbackURL: configService.get<string>('HOMEPAGE_URL') + '/auth/google/callback',
+      //callbackURL: configService.get<string>('HOMEPAGE_URL') + '/auth/google/callback',
+      callbackURL: 'http://koreauniv.gdgoc.kr/auth/google/callback',
       scope: ['email', 'profile'],
     });
   }
