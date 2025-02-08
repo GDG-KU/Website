716e574b253f49e511e0f00391d2daca17833c72
wintery39 <wintery39@naver.com>
2025-02-08 12:02:32 +0900

    get user with pagination

--- website_be/src/user/user.controller.ts
+++ website_be/src/user/user.controller.ts
@@ -1,9 +1,9 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Req } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards, Req, Query } from '@nestjs/common';
 import { UserService } from './user.service';
 import { CreateUserDto } from './dto/request/create-user.dto';
-import { ApiBearerAuth, ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiOperation, ApiQuery, ApiResponse } from '@nestjs/swagger';
 import { UpdateUserRoleDto } from './dto/request/update-user.role.dto';
-import { UserInfoResponseDto } from './dto/response/user.response.dto';
+import { UserInfoPaginatedResponseDto, UserInfoResponseDto } from './dto/response/user.response.dto';
 import { JwtAuthGuard } from 'src/auth/security/jwt.guard';
 import { AuthorityGuard } from 'src/auth/security/authority.guard';
 import { UpdateUserAuthorityDto } from './dto/request/update-user.authority.dto';
@@ -16,18 +16,20 @@ import { SetAuthority } from 'src/auth/security/authority.decorator';
 export class UserController {
   constructor(private readonly userService: UserService) {}
 
-  @Post()
-  @ApiOperation({ summary: '유저 생성 // 개발용'})
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.userService.create(createUserDto);
+  @Get()
+  @ApiOperation({ summary: '유저 조회' })
+  @SetAuthority('PointManager', 'CalendarManager', 'AttendanceManager', 'RoleManager', 'AuthorityManager')
+  @ApiResponse({
+    description: '유저 조회 성공',
+    type: [UserInfoPaginatedResponseDto],
+  })
+  @ApiQuery({ name: 'page', required: false, type: Number, default: 1 })
+  @ApiQuery({ name: 'role', required: false, type: String })
+  findAll(@Query('page') page: number, @Query('role') role: string): Promise<UserInfoPaginatedResponseDto> {
+    return this.userService.findAll(page, role);
   }
 
-  @Get("all")
-  @ApiOperation({ summary: '모든 유저 조회 // 개발용'})
-  findAll() {
-    return this.userService.findAll();
-  }
-
+  
   @Patch("role")
   @ApiOperation({ summary: '유저 역할 변경'})
   @SetAuthority('RoleManager')
